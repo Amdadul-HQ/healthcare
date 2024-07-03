6e6df9afca41e767ad65f554eec5e4fcca644c79
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-03 18:34:39 +0600

    responsive:nevber responsive

--- src/components/Shared/Button.jsx
+++ src/components/Shared/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoArrowUpRight } from "react-icons/go";
 const Button = ({text,className}) => {
     return (
-        <button className={`flex items-center gap-x-2 px-7 py-3 border rounded-xl ${className}`}>
+        <button className={`flex items-center gap-x-2 px-3 lg:px-7 lg:py-3 border rounded-xl ${className}`}>
             {text}<GoArrowUpRight className='text-base'/>
         </button>
     );
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,11 +1,13 @@
+'use client'
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import MaxWidth from './MaxWidth';
 import Button from './Button';
-
+import Hamburger from 'hamburger-react'
 
 
 const Navbar = () => {
+    const [isOpen, setOpen] = useState(false)
 const links = [
     {
         id:1,
@@ -27,17 +29,22 @@ const links = [
     return (
         <header>
             <MaxWidth>
-            <nav className='flex items-center justify-between py-12'>
+            <nav className='flex items-center justify-between lg:py-12 py-4 lg:px-0 px-4'>
                 <div>
                     <Image src={'/Media/logo dark.png'} width={125} height={33} alt=''/>
                 </div>
-                <ul className='flex gap-x-6'>
+                <ul className={`flex gap-x-6 lg:static fixed lg:flex-row flex-col transition-all duration-500 ${isOpen ? 'right-5 top-14':'-right-52 top-14'} lg:text-left text-right`}>
                     {
-                        links && links.map(route => <li className='text-primary text-base font-normal' key={route.id}>{route.title}</li>) 
+                        links && links.map(route => <li className='text-primary cursor-pointer text-base font-normal' key={route.id}>{route.title}</li>) 
                     }
+                    <Button text={'Appointment'} className={'border-primary border-2 lg:hidden text-primary font-medium'} />
+                    
                 </ul>
-                <div>
+                <div className='lg:flex hidden'>
                     <Button text={'Appointment'} className={'border-primary border-2 text-primary font-medium'} />
+                </div>
+                <div className='lg:hidden'>
+                <Hamburger toggled={isOpen} toggle={setOpen} />
                 </div>
             </nav>
             </MaxWidth>
