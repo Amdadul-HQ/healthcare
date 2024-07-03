328f6813f92be7be920cd4e516a5bcfd881276b0
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-03 18:09:53 +0600

    fix:fixing deployment issu

---
+++ src/app/homepage/page.jsx
@@ -0,0 +1,24 @@
+import Faq from '@/components/ui/Faq';
+import GetYourAppointment from '@/components/ui/GetYourAppointment';
+import Hero from '@/components/ui/Hero';
+import Service from '@/components/ui/Service';
+import Statistics from '@/components/ui/Statistics';
+import Testimonial from '@/components/ui/Testimonial';
+import WhoWeAre from '@/components/ui/WhoWeAre';
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <Hero/>
+      <Statistics/>
+      <WhoWeAre/>
+      <Service/>
+      <Testimonial/>
+      <Faq/>
+      <GetYourAppointment/>
+        </>
+    );
+};
+
+export default HomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,9 @@
-import Faq from "@/components/ui/Faq";
-import GetYourAppointment from "@/components/ui/GetYourAppointment";
-import Service from "@/components/ui/Service";
-import Statistics from "@/components/ui/Statistics";
-import Testimonial from "@/components/ui/Testimonial";
-import WhoWeAre from "@/components/ui/WhoWeAre";
-import Hero from "@/components/ui/hero";
-import Image from "next/image";
+import HomePage from "./homepage/page";
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
-      <Statistics/>
-      <WhoWeAre/>
-      <Service/>
-      <Testimonial/>
-      <Faq/>
-      <GetYourAppointment/>
+      <HomePage/>
     </main>
   );
 }
