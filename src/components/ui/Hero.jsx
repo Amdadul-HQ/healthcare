20368b280d516e75da5d4d431a12444d48c6c477
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-03 17:54:00 +0600

    adding some pack

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -6,10 +6,22 @@ const Hero = () => {
     return (
         <section>
             <MaxWidth>
-                <div className='w-full h-[470px] relative'>
+                {/* <div className='w-full h-[470px] relative'>
                <Image src='/Media/Rectangle5.png' className='z-0' width={1160} height={470} alt='banner'/>
                 
-                </div>
+                </div> */}
+                <div className="relative w-[1160px] h-[470px]">
+          <Image
+            className="rounded-[48px]"
+            src="/Media/Rectangle5.png"
+            width={1160}
+            height={477}
+            alt=""
+          />
+          <div className="absolute bg-gradient-to-t from-[#02004378] to-[#00C19D00] w-full h-full bottom-0 rounded-[48px]">
+           
+          </div>
+        </div>
             </MaxWidth>
         </section>
     );
